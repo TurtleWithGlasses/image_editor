2dd794c8aaaf6a1f04a1427d9815f7eb1478b5bc
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-27 17:57:55 +0300

    Create logic to implement effects

--- menu.py
+++ menu.py
@@ -41,7 +41,7 @@ class EffectsFrame(ctk.CTkFrame):
         self.pack(expand=True,fill="both")
 
         DropDownPanel(self,effect_vars["effect"],EFFECT_OPTIONS)
-        SliderPanel(self,"Blur",effect_vars["blur"],0,3)
+        SliderPanel(self,"Blur",effect_vars["blur"],0,30)
         SliderPanel(self,"Contrast",effect_vars["contrast"],0,10)
 
 
