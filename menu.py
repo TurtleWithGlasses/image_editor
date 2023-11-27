740b765bbd86407b89cac43d3a070d8ef6e80477
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-27 16:42:55 +0300

    Implement logic to manipulate the image

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 from panels import *
 
 class Menu(ctk.CTkTabview):
-    def __init__(self,parent):
+    def __init__(self,parent,rotation,zoom):
         super().__init__(parent)
         self.grid(row=0,column=0,sticky="nsew",pady=10,padx=10)
 
@@ -12,17 +12,17 @@ class Menu(ctk.CTkTabview):
         self.add("Effects")
         self.add("Export")
 
-        PositionFrame(self.tab("Position"))
+        PositionFrame(self.tab("Position"),rotation,zoom)
         ColorFrame(self.tab("Color"))
         EffectsFrame(self.tab("Effects"))
         ExportFrame(self.tab("Export"))
 
 class PositionFrame(ctk.CTkFrame):
-    def __init__(self,parent):        
+    def __init__(self,parent,rotation,zoom):        
         super().__init__(parent,fg_color="light grey")
         self.pack(expand=True,fill="both")
-        SliderPanel(self,"Rotation")
-        SliderPanel(self,"Zoom")
+        SliderPanel(self,"Rotation",rotation,0,360)
+        SliderPanel(self,"Zoom",zoom, 0, 200)
 
 class ColorFrame(ctk.CTkFrame):
     def __init__(self,parent):
