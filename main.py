bf6d0360be32db89cdfc84e316143a04bf07ed83
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-27 13:58:57 +0300

    Implement image import & display/scale functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from image_widgets import *
+from PIL import Image, ImageTk
 
 class App(ctk.CTk):
     def __init__(self):
@@ -14,15 +15,43 @@ class App(ctk.CTk):
         self.columnconfigure(0,weight=2)
         self.columnconfigure(1,weight=6)
 
-
         # widgets
-        ImageImport(self,self.import_image)
+        self.image_import = ImageImport(self,self.import_image)
+        
 
         # import button
 
         self.mainloop()
 
     def import_image(self,path):
-        print(path)
+        self.image = Image.open(path)
+        self.image_ratio = self.image.size[0] / self.image.size[1]
+
+        self.image_tk = ImageTk.PhotoImage(self.image)
+
+        self.image_import.grid_forget()
+        self.image_output = ImageOutput(self,self.resize_image)
+    
+    def resize_image(self,event):
+
+        # current canvas ratio
+        canvas_ratio = event.width / event.height
+
+        # resize
+        if canvas_ratio > self.image_ratio:
+            image_height = int(event.height)
+            image_width = int(image_height * self.image_ratio)
+        else:
+            image_width = int(event.width)
+            image_height = int(image_width / self.image_ratio)
+            
+
+        # place image
+        self.image_output.delete("all")
+        resized_image = self.image.resize((image_width,image_height))
+        self.image_tk = ImageTk.PhotoImage(resized_image)
+        self.image_output.create_image(event.width / 2,
+                                       event.height / 2,
+                                       image=self.image_tk)
 
 App()
